90d9e9d1da268013fb479994e529276b371bbbdb
Griego86 <dimi1086@hotmail.com>
2023-11-18 17:23:38 -0800

    tweeter messenger modifications

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    const $input = $(this);
+    const $text = $input.val();
+    const remainingChar = 140 - $text.length;
+    const $counterElem = $input.parent().find(".counter");
+    $counterElem.text(remainingChar);
+    $counterElem.css("color", "#545149");
+    
+    if (remainingChar < 0) {
+      $counterElem.css("color", "red");
+    }
+  });
+});
