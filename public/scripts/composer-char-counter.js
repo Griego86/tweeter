c49472ed5ced073184cfdfa9d91f072b960d14fe
Griego86 <dimi1086@hotmail.com>
2023-11-25 18:04:17 -0800

    fixed counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $("#tweeter-text").on("input", function() {
+  $("#tweet-text").on("input", function() {
     const $input = $(this);
     const $text = $input.val();
     const remainingChar = 140 - $text.length;
-    const $counterElem = $input.parent().find(".counter");
+    const $counterElem = $input.parent().find(".counter"); // Changed to ".counter" here
     $counterElem.text(remainingChar);
     $counterElem.css("color", "#545149");
     
